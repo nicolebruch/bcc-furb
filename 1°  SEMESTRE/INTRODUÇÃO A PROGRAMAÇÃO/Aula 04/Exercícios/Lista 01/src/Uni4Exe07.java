dbcf1c65f9bec4e2301a2be829595020b650e2cd
nicole <44910604+nicolebruch@users.noreply.github.com>
2025-03-03 13:50:40 -0300

    comittando os novos exercicios

--- 1°  SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 04/Exercícios/Lista 01/src/Uni4Exe07.java
+++ 1°  SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 04/Exercícios/Lista 01/src/Uni4Exe07.java
@@ -14,41 +14,40 @@ public class Uni4Exe07 {
          */
         Scanner entrada = new Scanner(System.in);
 
-System.out.println("Digite o peso da carta em gramas: ");
-double peso = entrada.nextDouble();
+        System.out.println("Digite o peso da carta em gramas: ");
+        double peso = entrada.nextDouble();
 
-/* fecho o scanner p/ não consumir memória */
-entrada.close();
+        /* fecho o scanner p/ não consumir memória */
+        entrada.close();
 
-/* declaro o valor do selo */
-double valorSeloCinquentaGramas = 0.45;
+        /* declaro o valor do selo */
+        double valorSeloCinquentaGramas = 0.45;
 
+        /*
+         * variavel p caso o selo ultrapasse de 50 gramas,
+         * com ela, o usuário irá pagar um adicional de R$ 0,45 por cada 20 gramas, ou
+         * fração, em que excedem aquele peso.
+         */
+        /*
+         * só utilizo essa variável para ver o quanto de peso ultrapassou de 50 gramas
+         */
+        double pesoSuperior = peso - 50;
 
+        /*fluxograma pede p dar false caso o peso seja excedido, 
+        então crio uma variável p verificar se o peso é válido ou não
+         */
+        boolean pesoValido = peso <= 50;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        /*crio uma condição para verificar se o peso é válido ou não */
+        if (pesoValido) {
+            /*caso sim, ele só paga o valor do selo de até 50 gramas */
+            System.out.println("O valor do selo é: R$ " + valorSeloCinquentaGramas);
+            /*se passar de 50 gramas, ele paga um preço superior, e divido por cada 20 gramas */
+        } else {
+            double valorSeloExcedente = (pesoSuperior / 20) * 0.45;
+            double valorPagar = valorSeloCinquentaGramas + valorSeloExcedente;
+            System.out.println("O valor do selo é: R$ " + valorPagar);
+        }
 
     }
 }
---
+++ 1°  SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 04/Exercícios/Lista 01/src/Uni4Exe08.java
@@ -0,0 +1,49 @@
+/*Grupo COMPOSTO E e OU (&& e ||) */
+/*Dado uma letra, escreva um algoritmo que informe se ela é ou não uma vogal.
+Para resolver este problema pode se utilizar do algoritmo descrito no fluxograma: */
+
+import java.util.Scanner;
+
+public class Uni4Exe08 {
+    public static void main(String[] args) {
+        /*
+         * Scanner pra poder estar puxando os
+         * dados na memória que o usuário informou
+         */
+        Scanner entrada = new Scanner(System.in);
+
+        /*pro usuário informar uma letra*/
+        System.out.println("Informe uma letra: ");
+        char letra = entrada.next().charAt(0);
+
+        /*aqui é pro método main conseguir mostrar a mensagem dizendo
+         * se é vogal ou se não é vogal
+         */
+        mostrarVogal(letra);
+
+        /* fecho o scanner p/ não consumir memória */
+        entrada.close();
+
+    }
+
+/*crio um método mais performático do que colocar uma condição enorme
+ * dentro de um if e else
+ */
+    private static boolean vogal(char letra) {
+        return Character.toUpperCase(letra) == 'A'
+                || Character.toUpperCase(letra) == 'E'
+                || Character.toUpperCase(letra) == 'I'
+                || Character.toUpperCase(letra) == 'O'
+                || Character.toUpperCase(letra) == 'U';
+
+    }
+
+    private static void mostrarVogal(char letra) {
+        if (vogal(letra)) {
+            System.out.println("A letra é uma vogal");
+        } else {
+            System.out.println("A letra não é uma vogal");
+        }
+
+    }
+}
