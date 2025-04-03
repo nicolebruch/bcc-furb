9a6501d03457681cabe580e0e722c029b6e80168
nicole <44910604+nicolebruch@users.noreply.github.com>
2025-04-03 20:50:40 -0300

    realizando exercicios

--- 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe02.java
+++ 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe02.java
@@ -1,3 +1,24 @@
+/*Descreva um algoritmo que calcule e escreva a soma dos números 
+pares e a soma dos números ímpares entre 1 e 100.
+ */
+
 public class Uni5Exe02 {
-    
+
+    public static void main(String[] args) {
+
+        int somaPar = 0;
+        int somaImpar = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                somaPar = somaPar + i;
+            } else {
+                somaImpar = somaImpar + i;
+            }
+        }
+
+        System.out.println("Soma par sendo:" + somaPar);
+        System.out.println("Soma ímpar sendo: " + somaImpar);
+
+    }
 }
---
+++ 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe03.java
@@ -0,0 +1,20 @@
+/*Descreva um algoritmo que calcule e 
+escreva a soma da seguinte série de 100 termos: */
+
+public class Uni5Exe03 {
+
+    
+    
+public static void main(String[] args) {
+    
+
+
+
+
+
+
+
+
+}
+
+}
