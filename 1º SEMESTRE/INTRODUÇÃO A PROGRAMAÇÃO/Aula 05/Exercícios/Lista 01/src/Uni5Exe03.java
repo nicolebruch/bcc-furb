545c0f00d5accb285d9742fbf4bdc41c3d5f4e45
nicole <44910604+nicolebruch@users.noreply.github.com>
2025-04-08 21:14:42 -0300

    realizando exercicios de for

--- 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe03.java
+++ 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe03.java
@@ -1,20 +1,20 @@
 /*Descreva um algoritmo que calcule e 
-escreva a soma da seguinte série de 100 termos: */
+escreva a soma da seguinte série de 100 termos: 
+1/1 + 1/2 + 1/3 + 1/4 + ... + 1/100*/
+
+//precisa usar o FOR
 
 public class Uni5Exe03 {
+    public static void main(String[] args) {
 
-    
-    
-public static void main(String[] args) {
-    
+        double soma = 0;
+        double i = 1;
 
+        for (i = 1; i <= 100; i++) {
+            soma = soma + (1 / i);
+        }
+        System.out.println("A soma da série é: " + soma);
 
-
-
-
-
-
-
-}
+    }
 
 }
---
+++ 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe04.java
@@ -0,0 +1,24 @@
+/*Descreva um algoritmo para calcular o valor de S dado por:
+
+S = 3/2 + 5/6 + 7/12 + 9/20 + 11/30 + ...
+
+Considere os 20 primeiros termos da série.
+
+Pense no resultado!*/
+
+public class Uni5Exe04 {
+    public static void main(String[] args) {
+
+        double soma = 0;
+        double numerador = 3;
+        double denominador = 2;
+
+        for (int i = 1; i <= 20 i++) {
+            soma = soma + (numerador / denominador);
+            numerador = numerador + 2; 
+            denominador = denominador + i;
+        }
+        System.out.println("A soma da série é: " + soma);
+    }
+
+}
