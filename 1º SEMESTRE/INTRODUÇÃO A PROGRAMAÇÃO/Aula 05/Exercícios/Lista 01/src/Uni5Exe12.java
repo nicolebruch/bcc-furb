5c967404fd0c975d243de514383ce23186dd08e3
nicole <44910604+nicolebruch@users.noreply.github.com>
2025-04-13 21:59:13 -0300

    realizando exercicios de prog

--- 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe12.java
+++ 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 05/Exercícios/Lista 01/src/Uni5Exe12.java
@@ -3,14 +3,12 @@ e em seguida imprima n linhas do chamado Triangulo de Floyd:
 
 Triangulo de Floyd
 
-
 1  
 2   3   
 4   5   6
 7   8   9   10
 11  12  13  14  15
 16  17  18  19  20  21
-
 
 Pense no resultado! */
 
@@ -27,6 +25,7 @@ public class Uni5Exe12 {
                 System.out.print("\t" + numeros);
                 numeros++;
             }
+
             System.out.println();
         }
         ler.close();
