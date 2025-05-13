3b14c9a6975ebaa8bf96e48195eb651930e69c2b
Nicole Bruch <44910604+nicolebruch@users.noreply.github.com>
2025-05-13 16:37:52 +0000

    realizando exerc de prog

---
+++ 1º SEMESTRE/INTRODUÇÃO A PROGRAMAÇÃO/Aula 06/Exercícios/Lista 01/src/Uni6Exe07.java
@@ -0,0 +1,42 @@
+/*Faça um programa que leia um valor N inteiro limitado a 20 posições. 
+Com base neste valor, crie um vetor do tipo inteiro. 
+Em seguida, solicite ao usuário para digitar um número inteiro várias vezes, até preencher o vetor. 
+Esse número deverá ser armazenado no vetor caso ainda não exista, e se o valor já existir deve ser pedido um novo valor. 
+Isto é, se o usuário informar o número 2 e já existir o número 2 em alguma posição do vetor, o número não deve ser adicionado. 
+Após ter adicionado todos os números dentro do vetor, o mesmo deve ser ordenado. 
+Para isso reordene os elementos internos do vetor de modo que este fique em ordem crescente, conforme no exemplo: OBSERVAÇÃO: Não utilize o Arrays.sort
+
+Vetor origem: [0][4][2][6][3]
+Vetor ordenado: [0][2][3][4][6]
+
+Crie um método para inserir os valores no vetor, 
+outro para ordenar o vetor e outro para informar o vetor resultante. 
+Para "método ordenar" se pode usar o "método bolha" explicado neste vídeo. */
+
+//ler valor N inteiro (no maximo 20 posicoes)
+//criar um vetor do tipo inteiro com os valores lidos 
+//user deve digitar numeros inteiros ate preencher as posicoes existentes do vetor 
+//se o numero informado ainda nao ter sido informado antes, add no vetor
+//se o numero informado ja ter sido informado antes, nao add no vetor e pede outro numero
+//depois de formar o vetor precisa ordenar ele 
+//precisa ser ordenado em forma crescente 
+
+//criar metodo p inserir valor no vetor
+//criar metodo p ordenar o vetor (usar o metodo bolha)
+//criar metodo p informar o resultado do vetor
+
+import java.util.Scanner;
+
+public class Uni6Exe07 {
+ public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+
+
+
+
+}
